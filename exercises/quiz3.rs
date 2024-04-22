f923911464619248e48380c825822a326c7495c2
Alex Ji <alji@cisco.com>
2024-04-21 20:06:38 -0500

    reached iterator but have not started

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,10 +16,25 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+pub trait Grade {
+    fn get_grade(&self) -> String;
+}
+
+impl Grade for f32 {
+    fn get_grade(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Grade for String {
+    fn get_grade(&self) -> String {
+        self.to_string()
+    }
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Box<dyn Grade>,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -27,7 +42,7 @@ pub struct ReportCard {
 impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.grade.get_grade())
     }
 }
 
@@ -38,7 +53,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Box::new(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -51,8 +66,9 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
+            let s = String::from("A+");
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Box::new(s),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
